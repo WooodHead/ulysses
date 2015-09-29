3350769421fba57d673a0d12488030ac1c6c85fd
Roman Klauke <romankl@users.noreply.github.com>
2015-09-29 17:05:57 +0200

    simpler controller mounting

--- src/routes/bootstrap.js
+++ src/routes/bootstrap.js
@@ -4,23 +4,13 @@ const Express = require('express');
 module.exports = function (env) {
     fs.readdirSync(__dirname + '/controller').forEach(function (name) {
         const controller = require('./controller/' + name);
+
         const app = Express();
-
-
         app.set('view engine', 'jade');
         app.set('views', __dirname + '/controller/' + name + '/views');
-
-        const definitions = controller.definition;
-
-        for (var i = 0; i < definitions.length; i++) {
-            const element = definitions[i];
-            const method = element.method;
-            const path = element.path;
-            const handler = element.handler;
-            const isSecure = element.isSecure || false;
-
-            app[method](path, handler);
-        }
+        app.use(controller.router);
         env.use(app);
+
+        console.log('mounted controller ' + name);
     });
 };
